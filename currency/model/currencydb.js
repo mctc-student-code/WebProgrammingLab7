7ccee159559fca1203d25ebd6311e69aee1028c3
mvangerpen <mark.vangerpen@gmail.com>
2018-04-15 13:20:21 -0500

    Lab 11 Advanced - Issue with Callback in currencydb.js

--- currency/model/currencydb.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    dollars: 1.0,
-    euros: 0.85,
-    rupees: 4.5
-}
---
+++ model/currencydb.js
@@ -0,0 +1,30 @@
+var request = require('request');
+var baseURL = 'http://data.fixer.io/api/';
+
+var APIKEY = process.env.FIXER_API;
+
+function currencyRequest(callback, base, to) {
+
+    // Wait for callback
+    process.nextTick(function() {
+        // Include API key in parameters
+        queryParam = { 'access_key': APIKEY, 'base': base, 'symbols': to };
+
+        // Request module will request data
+        request({ uri: baseURL, qs: queryParam }, function(error, fixer_response, body) {
+
+            if (!error && fixer_response.statusCode == 200) {
+                console.log("FIXER RESPONSE: \n" + JSON.stringify(body));
+                var fixerJSON = JSON.parse(body); // Convert to js object
+                callback(null, fixerJSON);
+            } else {
+                // Log errors
+                console.log("Error in request: " + error);
+                console.log(fixer_response);
+                callback(Error("Error fetching data"));
+            }
+        });
+    });
+}
+
+module.exports = currencyRequest;
